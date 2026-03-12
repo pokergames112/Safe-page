890758be431d55d2bef36a43a05a6f7bfcd622b1
João Lucas <joao.lsf98@gmail.com>
2026-03-12 16:25:30 -0300

    novo código script

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnAcesso = document.getElementById("btn-acesso");
+
+    btnAcesso.addEventListener("click", (e) => {
+        // Previne que a página recarregue à toa
+        e.preventDefault(); 
+        
+        // Coloque o seu link de vendas real entre as aspas abaixo:
+        window.location.href = "https://www.trevo.club/";
+    });
+});
